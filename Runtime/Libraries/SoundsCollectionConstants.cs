3c9d410a86fa1e9c13481350e37ae1ce78b83fe5
Denis Puzik <denkacn@gmail.com>
2024-11-24 22:08:37 +0200

    remove not used

--- Runtime/Libraries/SoundsCollectionConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace SoundFlowSystem.Libraries
-{
-    public static class SoundsCollectionConstants
-    {
-        public static readonly string TestSfxPlay = "test_sfx_play";
-        public static readonly string DestroyRedItemKey = "destroy_red_item_key";
-    }
-}
